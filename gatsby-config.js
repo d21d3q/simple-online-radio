f589184d8813d998e9bafa5710c8871806f569e6
Zdzisław Krajewski <zdzislaw@krw.sk>
2022-01-28 16:39:11 +0100

    Switch to preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
+      `gatsby-plugin-preact`,
       `gatsby-plugin-postcss`,
       `gatsby-plugin-image`,
       `gatsby-plugin-sharp`,
